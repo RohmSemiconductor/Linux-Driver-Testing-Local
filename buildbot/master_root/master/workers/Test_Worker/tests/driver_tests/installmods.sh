99f39a2386009d7758c32c1b1d9c522a6889af11
Topi Sonkajärvi <sonkajarvi@hotmail.com>
2026-03-11 09:59:04 +0200

    import kernel

--- buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/installmods.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/user01/Linux-Driver-Testing/buildbot/master_root/sandbox/bin/activate
-pytest -ra test_000_login.py --beagle=beagle$2 --power_port=$3 --type=PMIC --product=$1
-pytest --lg-env beagle$2.yaml test_001_init_overlay.py
-pytest -ra --lg-env beagle$2.yaml test_002_merge_dt_overlay.py --product=$1
-pytest -ra --lg-env beagle$2.yaml test_003_insmod_tests.py --product=$1
-pytest -ra --lg-env beagle$2.yaml test_004_init_regulator_test.py --product=$1
-#pytest -ra --lg-env beagle1.yaml test_test_target.py --product=$1 -W ignore::DeprecationWarning
-
