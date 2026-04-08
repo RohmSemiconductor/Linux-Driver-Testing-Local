fd77c9ed5d804a4261c1cf73966d6d4c17b59457
Topi Sonkajärvi <sonkajarvi@hotmail.com>
2026-04-08 15:30:58 +0300

    Update test_002_merge_dt_overlay.py

--- buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/test_002_merge_dt_overlay.py
+++ buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/test_002_merge_dt_overlay.py
@@ -1,34 +1,34 @@
 import pytest
 import sys
-
-sys.path.append('..')
-sys.path.append('./configs')
-from test_util import *
-
 from time import sleep
+
+sys.path.append("..")
+sys.path.append("./configs")
+from test_util import *
 from kernel_modules import *
 
-def test_merge_dt_overlay(command,product, result_dir):
-    result['type'] = 'generic'
-    result['stage'] = 'merge_dt_overlay'
-    result['result_dir'] = result_dir
+def test_merge_dt_overlay(command, product, result_dir):
+    result["type"] = "generic"
+    result["stage"] = "merge_dt_overlay"
+    result["result_dir"] = result_dir
 
-    for x in range(0,len(kernel_modules['dt_overlays'][product])):
-        stdout, stderr, returncode = command.run('dd if=/'+kernel_modules['dt_overlays'][product][x]+' of=/sys/kernel/mva_overlay/overlay_add bs=1M')
-    if (returncode != 0):
-        print(stdout[-1])
+    overlays = kernel_modules["dt_overlays"][product]
+    for overlay in overlays:
+        stdout, stderr, returncode = command.run(f"dd if=/{overlay} of=/sys/kernel/mva_overlay/overlay_add bs=1M")
+        if returncode != 0:
+            print(stdout[-1])
 
-    # Sleep is needed because kernel functions take a bit of time
-    # to actually load the modules after dd command completes.
-    # sleep(2) is enough for now, increase if this test fails
-    # for this reason. To manually check false negative, run
-    # command lsmod after running the dd command to see if the
-    # modules are loaded.
+    # Sleep is needed here because kernel functions take a bit of time to
+    # actually load the modules after dd'ing. 'sleep(2)' is enough for now;
+    # increase if this test fails. To manually check for false negatives,
+    # run 'lsmod' after running 'dd' to see if the modules are loaded.
     sleep(2)
-    lsmod, stderr, returncode = command.run('lsmod')
-    for x in range(len(kernel_modules['merged_dt_overlay'][product])):
-        result['expect'].append([kernel_modules['merged_dt_overlay'][product][x], 0])
-        result['return'].append([kernel_modules['merged_dt_overlay'][product][x], checkStdOut(lsmod,kernel_modules['merged_dt_overlay'][product][x])])
 
-    result['lsmod'] = lsmod
+    stdout, stderr, returncode = command.run("lsmod")
+    merged_overlays = kernel_modules["merged_dt_overlay"][product]
+    for overlay in merged_overlays:
+        result["expect"].append([overlay, 0])
+        result["return"].append([overlay, checkStdOut(stdout, overlay)])
+
+    result["lsmod"] = stdout
     check_result(result)
