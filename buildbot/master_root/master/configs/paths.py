46193665ad3953772cff192b8524b505958c9b14
Topi Sonkajärvi <sonkajarvi@hotmail.com>
2026-04-10 11:43:53 +0300

    Configs: add entries for new options

--- buildbot/master_root/master/configs/paths.py
+++ buildbot/master_root/master/configs/paths.py
@@ -2,3 +2,18 @@ ARM32_COMPILER_PATH = ""
 NFS_PATH = ""
 TFTPBOOT_PATH = ""
 WORKER_ROOT_PATH = ""
+
+ENABLE_IRC_REPORT = False
+ENABLE_GIT_BISECT = False
+
+ENABLE_LOCALHOST = True
+URLS = {
+    "default": "",
+    "localhost": "http://localhost:8010",
+}
+
+FACTORIES = [
+    # "pmic",
+    # "accelerometer",
+    # "addac",
+]
