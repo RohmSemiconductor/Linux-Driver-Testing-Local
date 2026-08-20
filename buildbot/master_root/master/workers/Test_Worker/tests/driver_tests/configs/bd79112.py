9bc47e2067604228fdc464bbb45c8a2208f82d2e
Topi Sonkajärvi <sonkajarvi@hotmail.com>
2026-08-20 10:36:48 +0300

    BD79112: add config and device tree template

---
+++ buildbot/master_root/master/workers/Test_Worker/tests/driver_tests/configs/bd79112.py
@@ -0,0 +1,14 @@
+data = {
+    "name": "bd79112",
+
+    "gpio": {
+        "in0": {
+            "label": "bd79112-gpio",
+            "index": 0,
+        },
+        "in1": {
+            "label": "bd79112-gpio",
+            "index": 1,
+        },
+    }
+}
